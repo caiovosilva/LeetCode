d8fc0302f1ef428e9a2eda66f9acbcc16ee6bd6b
Caio Silva <caiovosilva@outlook.com>
2024-08-19 13:55:23 -0300

    add project; first two questions

---
+++ Leetcode75/Questions/GcdOfStrings.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Leetcode75.Questions;
+
+public partial class LeetCode75
+{
+    public static string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1) 
+            return string.Empty;
+        
+        var lengthOfCommonString = BigInteger.GreatestCommonDivisor(str1.Length, str2.Length);
+        return str1[..(int)lengthOfCommonString]; // same as str1.Substring(0, lengthOfCommonString);
+    }
+}
---
+++ Leetcode75/Questions/MergeStringsAlternately.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode75.Questions;
+
+public partial class LeetCode75
+{
+    public static string MergeAlternately(string word1, string word2)
+    {
+        var result = string.Empty;
+        var longestString = Math.Max(word1.Length, word2.Length);
+        for (var i = 0; i < longestString; i++)
+        {
+            if(word1.ElementAtOrDefault(i) != default)
+            {
+                result += word1[i];
+            }
+            if(word2.ElementAtOrDefault(i) != default)
+            {
+                result += word2[i];
+            }
+        }
+
+        return result;
+    }
+}

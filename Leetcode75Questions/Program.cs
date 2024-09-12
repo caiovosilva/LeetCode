40012d691f15554d585a44d4316b210bcb32f1f1
Caio Silva <caiovosilva@outlook.com>
2024-09-12 12:28:52 -0300

    RemoveElement

--- Leetcode75Questions/Program.cs
+++ Leetcode75Questions/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Leetcode75;
 
 Console.WriteLine("Hello, World!");
-var result1 = Questions.GcdOfStrings("ABCABC", "ABC");
+var result1 = Questions.RemoveElement([3,2,2,3], 3);
 Console.WriteLine(result1);
-var result2 = Questions.CanPlaceFlowers([1,0,0,0,1], 1);
-Console.WriteLine(result2);
 Console.ReadLine();
---
+++ Leetcode75Questions/RemoveElement.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode75;
+
+public abstract partial class Questions
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        var k = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val) continue;
+
+            nums[k] = nums[i];
+            k++;
+        }
+        
+        //
+        // var valIndexes = new Queue<int>();
+        // var nonValCount = 0;
+        // for (var i = 0; i < nums.Length; i++)
+        // {
+        //     if (nums[i] == val)
+        //     {
+        //         valIndexes.Enqueue(i);
+        //     }
+        //     else
+        //     {
+        //         nonValCount++;
+        //     }
+        // }
+        //
+        // for (var i = nums.Length - 1; i >= 0 && valIndexes.Count > 0; i--)
+        // {
+        //     if (nums[i] == val) continue;
+        //     
+        //     var valIndex = valIndexes.Dequeue();
+        //     nums[valIndex] = nums[i];
+        // }
+
+        return k;
+    }
+}
